6cf9dc6c770778b62ebc7fbc8674eca6333c6baf
Pant <dipant@ciena.com>
2021-02-19 19:04:57 +0530

    first commit

---
+++ src/main/java/com/ppmtoolfullstack/PPMToolFullstack/PpmToolFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.ppmtoolfullstack.PPMToolFullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppmtoolfullstack/PPMToolFullstack/PpmToolFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppmtoolfullstack.PPMToolFullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
